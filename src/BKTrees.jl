ec58e4d2d4d036e9a8ccaa5ce948a581db0791d0
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-10-01 10:12:47 +0200

    Added ascii art

--- src/BKTrees.jl
+++ src/BKTrees.jl
@@ -1,3 +1,29 @@
+##############################################################
+#oxdlcccloloollllcccccclollcclodooodxkkxdddxxddollc:::::;;;;;#
+#dkxdoooodddoolllcllloollllllodddddddxkOOkxxxxxxxxoccc:::c:::#
+#xxdddxkkxdloollloddddoollol:;,;;lkOOkxxdddddxdxkkdollcccc:::#
+#dxkxxxxkxxdooodxxxool,;,:;;'.',;;,cl;;coxxxxxkkkkkkxxdolcccc#
+#kOkxkxxkkOkxxxkOOdl;.,,',',:;,;,,;;,::;;::oOkOOOOOkxdooollcc#
+#OkkkkkO0KOOO0KKK0c:..'.',',,..,;,,;;;;,;;:l0KKXKKKK0OOkxdool#
+#KKK00OkOkkO0KKKKdc;',',;::,',.;:,'..',;,'',;x0KXKKKK00OO0Okx#
+#OOO000OOO0OO0kkd::;,,;,'';,.''.....',,,,..',:OOxk00K0K00K00O#
+#K0000KKKKK00dc:;;,:.....';;;cc.....,;'.,,'',:;cc,dXXXXXKKK0O#
+#XXXXXXXKKKKKd;',;;:..'',...',. .......,,'.',,;;,,lNNXXXXXKK0#
+#XXNNNNNNXXkl..,,'',....,,'.',,.'.......;::,:;';,':x0XXKXXXXK#
+#00000KKXXXd;'';,'.,;..''.;..';'',,''..,'',';;;'',,;l00XXXKK0#
+#KKKKXXXKKKKo':od,.ll .,','. .''..':o:........',,,,cckKNNXXXX#
+#KKKKKKKKKko::lc;,'.'.....',... .,;.'''.';'..''..,;coKXNNNNXX#
+#XXXXXXXXkc,;,,cc:;,,:''......;..,;:,.,;l:c,;;.;c;lxXXNNNNNNN#
+#XXXXXXXXX0cdcl:co,;;':ll,''..:.,'';:,.cxOxl,:c:0NWWWNXXXXXXN#
+#NNNNNNNNNNNNXxkNNKOOXNNNX0K0kk.,o:clcxdONNXKoKNWNNNNNNNXXNNN#
+#WWWWWWWWWWWWWWWWWWWWWWWWWWWWWK..NNNNNNWWWWNNNNNNNWNNNNNNNNNN#
+#NNNNXXXXXXXKKKKKKK0kdxkxkxxdxl::dxdxxxxkOkkO0KXXXXXXKKKKKKKK#
+#dddxxxxxxddddddxddddddddddddddddddooodoooolllllollllllllllll#
+##############################################################
+
+# BKTrees.jl - Burkhard-Keller trees implementation written
+#              at 0x0α Research by Corneliu Cofaru, 2018
+
 module BKTrees
 
 using DataStructures: Deque
